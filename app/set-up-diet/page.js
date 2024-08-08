6cddeb4f08fc9f2f454a7e74110ae5c4fec5a172
GUILHERMEMMARTINEZ <99258374+GUILHERMEMMARTINEZ@users.noreply.github.com>
2024-08-07 23:57:30 -0600

    update 8

--- app/set-up-diet/page.js
+++ app/set-up-diet/page.js
@@ -4,7 +4,15 @@ import Link from 'next/link';
 import DietTable from './DietTable';
 import { Suspense } from 'react';
 
-export default function Home() {
+const SetupDietPage = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <DietSetupContent />
+    </Suspense>
+  );
+};
+
+const DietSetupContent = () => {
   const searchParams = useSearchParams();
   const bmr = searchParams.get('bmr');
   const tdee = searchParams.get('tdee');
@@ -24,18 +32,16 @@ export default function Home() {
         </div>
       </header>
 
-      <Suspense fallback={<div>Loading...</div>}>
-        <div className="mt-16 w-full max-w-2xl bg-black bg-opacity-90 p-4 rounded-lg text-center text-white">
-          <h2 className="text-2xl font-semibold mb-4">Diet Setup</h2>
-          {bmr && tdee && (
-            <div className="bg-info p-4 mt-4 text-center font-semibold text-lg bg-black text-white rounded-lg">
-              <div>Your Basal Metabolic Rate (BMR): <b className="label label-success">{bmr}</b></div>
-              <div>Your Total Daily Energy Expenditure (TDEE): <b className="label label-success">{tdee}</b></div>
-            </div>
-          )}
-          <DietTable />
-        </div>
-      </Suspense>
+      <div className="mt-16 w-full max-w-2xl bg-black bg-opacity-90 p-4 rounded-lg text-center text-white">
+        <h2 className="text-2xl font-semibold mb-4">Diet Setup</h2>
+        {bmr && tdee && (
+          <div className="bg-info p-4 mt-4 text-center font-semibold text-lg bg-black text-white rounded-lg">
+            <div>Your Basal Metabolic Rate (BMR): <b className="label label-success">{bmr}</b></div>
+            <div>Your Total Daily Energy Expenditure (TDEE): <b className="label label-success">{tdee}</b></div>
+          </div>
+        )}
+        <DietTable />
+      </div>
 
       <footer className="fixed bottom-0 w-full bg-gray-800 bg-opacity-80 text-white flex justify-between items-center p-4">
         <nav className="flex space-x-4 footer-nav">
@@ -49,4 +55,6 @@ export default function Home() {
       </footer>
     </main>
   );
-}
+};
+
+export default SetupDietPage;
